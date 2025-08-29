7b2205e8c0de8cb02c0f2cf5337adff7d7edfbc5
Cyberphinx <88593809+Cyberphinx@users.noreply.github.com>
2025-08-29 23:52:15 +0100

    refactor: cli chatbot

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use dotenvy::dotenv;
-use rig::prelude::*;
+use rig::{cli_chatbot::cli_chatbot, prelude::*};
 use rig::{completion::Prompt, providers};
 
 #[tokio::main]
@@ -10,15 +10,18 @@ async fn main() -> Result<(), anyhow::Error> {
     let client = providers::ollama::Client::new();
 
     // Create agent with a single context prompt
-    let comedian_agent = client
+    let agent = client
         .agent("qwen2.5:14b")
-        .preamble("You are a comedian here to entertain the user using humour and jokes.")
+        .preamble("You are a super intelligent robot trying to achieve tehcnical perfection in providing help to coding challenges.")
         .build();
 
     // Prompt the agent and print the response
-    let response = comedian_agent.prompt("Entertain me!").await?;
+    // let response = comedian_agent.prompt("Entertain me!").await?;
 
-    println!("{response}");
+    // println!("{response}");
+
+    // Launch the interactive CLI chatbot REPL with the agent
+    cli_chatbot(agent).await?;
 
     Ok(())
 }
